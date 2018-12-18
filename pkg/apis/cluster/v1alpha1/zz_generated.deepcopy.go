94139792e0616e75fa4de5c59596b0faea6aa98f
Maisem Ali <3953239+maisem@users.noreply.github.com>
2018-12-18 10:43:07 -0800

    Adding ProviderID.

--- pkg/apis/cluster/v1alpha1/zz_generated.deepcopy.go
+++ pkg/apis/cluster/v1alpha1/zz_generated.deepcopy.go
@@ -710,6 +710,11 @@ func (in *MachineStatus) DeepCopyInto(out *MachineStatus) {
 		*out = new(string)
 		**out = **in
 	}
+	if in.ProviderID != nil {
+		in, out := &in.ProviderID, &out.ProviderID
+		*out = new(string)
+		**out = **in
+	}
 	return
 }
 
