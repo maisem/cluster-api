5b5ab29c0f00c3a17213fe55f71f1903bf013b64
Maisem Ali <maisem@google.com>
2018-12-19 09:58:39 -0800

    rework the node machine linker to use provider id

--- pkg/controller/node/node_controller.go
+++ pkg/controller/node/node_controller.go
@@ -18,11 +18,14 @@ package node
 
 import (
 	"context"
+	"sync"
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/klog"
+	clusterv1 "sigs.k8s.io/cluster-api/pkg/apis/cluster/v1alpha1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
@@ -38,51 +41,56 @@ func Add(mgr manager.Manager) error {
 }
 
 // newReconciler returns a new reconcile.Reconciler
-func newReconciler(mgr manager.Manager) reconcile.Reconciler {
-	return &ReconcileNode{
-		Client:          mgr.GetClient(),
-		scheme:          mgr.GetScheme(),
-		linkedNodes:     map[string]bool{},
-		cachedReadiness: map[string]bool{},
+func newReconciler(mgr manager.Manager) *Reconciler {
+	return &Reconciler{
+		Client: mgr.GetClient(),
+		scheme: mgr.GetScheme(),
+		links:  make(map[string]nodeMachineMapping),
 	}
+}
+
+type nodeMachineMapping struct {
+	providerID  string
+	nodeName    types.NamespacedName
+	machineName types.NamespacedName
 }
 
 // add adds a new Controller to mgr with r as the reconcile.Reconciler
-func add(mgr manager.Manager, r reconcile.Reconciler) error {
+func add(mgr manager.Manager, r *Reconciler) error {
 	// Create a new controller
-	c, err := controller.New("node-controller", mgr, controller.Options{Reconciler: r})
+	nc, err := controller.New("node-controller", mgr, controller.Options{Reconciler: reconcile.Func(r.ReconcileNode)})
 	if err != nil {
 		return err
 	}
-
 	// Watch for changes to Node
-	err = c.Watch(&source.Kind{Type: &corev1.Node{}}, &handler.EnqueueRequestForObject{})
-	if err != nil {
+	if err := nc.Watch(&source.Kind{Type: &corev1.Node{}}, &handler.EnqueueRequestForObject{}); err != nil {
 		return err
 	}
 
+	mc, err := controller.New("node-controller-machine", mgr, controller.Options{Reconciler: reconcile.Func(r.ReconcileMachine)})
+	if err != nil {
+		return err
+	}
+	// Watch for changes to Machines.
+	if err := mc.Watch(&source.Kind{Type: &clusterv1.Machine{}}, &handler.EnqueueRequestForObject{}); err != nil {
+		return err
+	}
 	return nil
 }
 
-var _ reconcile.Reconciler = &ReconcileNode{}
-
 // ReconcileNode reconciles a Node object
-type ReconcileNode struct {
+type Reconciler struct {
 	client.Client
 	scheme *runtime.Scheme
-
-	linkedNodes     map[string]bool
-	cachedReadiness map[string]bool
+	m      sync.RWMutex
+	links  map[string]nodeMachineMapping
 }
 
-// Reconcile reads that state of the cluster for a Node object and makes changes based on the state read
-// and what is in the Node.Spec
-// +kubebuilder:rbac:groups=core,resources=nodes,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=cluster.k8s.io,resources=machines,verbs=get;list;watch;create;update;patch;delete
-func (r *ReconcileNode) Reconcile(request reconcile.Request) (reconcile.Result, error) {
-	// Fetch the Node instance
-	instance := &corev1.Node{}
-	err := r.Get(context.Background(), request.NamespacedName, instance)
+func (r *Reconciler) ReconcileMachine(request reconcile.Request) (reconcile.Result, error) {
+	// Fetch the Machine instance
+	m := &clusterv1.Machine{}
+	ctx := context.Background()
+	err := r.Get(ctx, request.NamespacedName, m)
 	if err != nil {
 		if errors.IsNotFound(err) {
 			// Object not found, return.  Created objects are automatically garbage collected.
@@ -93,9 +101,134 @@ func (r *ReconcileNode) Reconcile(request reconcile.Request) (reconcile.Result, 
 		// Error reading the object - requeue the request.
 		return reconcile.Result{}, err
 	}
-
-	if instance.DeletionTimestamp.IsZero() {
-		return reconcile.Result{}, r.link(instance)
+	if m.Status.ProviderID == nil {
+		return reconcile.Result{}, nil
 	}
-	return reconcile.Result{}, r.unlink(instance)
+	id := *m.Status.ProviderID
+	klog.Infof("Reconciling machine %q", id)
+	if !m.DeletionTimestamp.IsZero() {
+		klog.Infof("Machine %q deleted, removing link", id)
+		// Nothing to do here, just remove the link.
+		r.m.Lock()
+		delete(r.links, id)
+		r.m.Unlock()
+		return reconcile.Result{}, nil
+	}
+
+	// See if we have already seen the node.
+	r.m.RLock()
+	x, ok := r.links[id]
+	r.m.RUnlock()
+	if !ok {
+		klog.Infof("First observation of machine %q", id)
+		// This means we haven't seen a node associated with this provider id yet.
+		r.m.Lock()
+		r.links[id] = nodeMachineMapping{
+			providerID:  id,
+			machineName: request.NamespacedName,
+		}
+		r.m.Unlock()
+		return reconcile.Result{}, nil
+	}
+	if x.nodeName.Name == "" {
+		klog.Infof("Node %q hasn't been observed yet", id)
+		// This means we haven't seen a node associated with this provider id yet.
+		return reconcile.Result{}, nil
+	}
+	if x.machineName.Name != "" {
+		klog.Infof("Machine and node already linked: %q", id)
+		// This means that we have already linked the machine and node.
+		return reconcile.Result{}, nil
+	}
+	node := &corev1.Node{}
+	if err := r.Get(ctx, x.nodeName, node); err != nil {
+		if errors.IsNotFound(err) {
+			// Don't do anything, let the node reconciliation loop deal with this.
+			return reconcile.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return reconcile.Result{}, err
+	}
+	klog.Infof("Linking %q", id)
+	if err := r.link(ctx, m, node); err != nil {
+		klog.Infof("Failed to link %q: %v", id, err)
+		return reconcile.Result{}, err
+	}
+	x.machineName = request.NamespacedName
+	r.m.Lock()
+	r.links[id] = x
+	r.m.Unlock()
+	return reconcile.Result{}, nil
+}
+
+// Reconcile reads that state of the cluster for a Node object and makes changes based on the state read
+// and what is in the Node.Spec
+// +kubebuilder:rbac:groups=core,resources=nodes,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=cluster.k8s.io,resources=machines,verbs=get;list;watch;create;update;patch;delete
+func (r *Reconciler) ReconcileNode(request reconcile.Request) (reconcile.Result, error) {
+	// Fetch the node instance
+	node := &corev1.Node{}
+	ctx := context.Background()
+	err := r.Get(ctx, request.NamespacedName, node)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Object not found, return.  Created objects are automatically garbage collected.
+			// For additional cleanup logic use finalizers.
+			klog.Errorf("Unable to retrieve Node %v from store: %v", request, err)
+			return reconcile.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return reconcile.Result{}, err
+	}
+	id := node.Spec.ProviderID
+	klog.Infof("Reconciling node with providerID: %q", id)
+	// See if we have already seen the machine.
+	r.m.RLock()
+	x, ok := r.links[id]
+	r.m.RUnlock()
+	if !ok {
+		klog.Infof("First observation of node %q", id)
+		// This means we haven't seen a machine associated with this provider id yet.
+		r.m.Lock()
+		r.links[id] = nodeMachineMapping{
+			providerID: id,
+			nodeName:   request.NamespacedName,
+		}
+		r.m.Unlock()
+		return reconcile.Result{}, nil
+	}
+	if x.machineName.Name == "" {
+		klog.Infof("Machine %q hasn't been observed yet", id)
+		// This means we haven't seen a machine associated with this provider id yet.
+		return reconcile.Result{}, nil
+	}
+	machine := &clusterv1.Machine{}
+	if err := r.Get(ctx, x.machineName, machine); err != nil {
+		if errors.IsNotFound(err) {
+			// Don't do anything, let the machine reconciliation loop deal with this.
+			return reconcile.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return reconcile.Result{}, err
+	}
+	if x.nodeName.Name != "" {
+		// This means that we have already linked the machine and node.
+		if !node.DeletionTimestamp.IsZero() {
+			klog.Infof("Node %q is being deleted", id)
+			// We should remove the link if the node goes away.
+			return reconcile.Result{}, r.unlink(ctx, machine, node)
+		}
+		klog.Infof("Machine and node already linked: %q", id)
+		return reconcile.Result{}, nil
+	}
+	klog.Infof("Linking %q", id)
+	if err := r.link(ctx, machine, node); err != nil {
+		klog.Infof("Failed to link %q: %v", id, err)
+		return reconcile.Result{}, err
+	}
+	x.nodeName = request.NamespacedName
+	r.m.Lock()
+	r.links[id] = x
+	r.m.Unlock()
+	return reconcile.Result{}, nil
 }
