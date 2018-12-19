65c6f80281330a98e7014395e5f40578bef1681c
Maisem Ali <3953239+maisem@users.noreply.github.com>
2018-12-19 09:58:39 -0800

    rework the node machine linker to use provider id

--- pkg/controller/node/node_controller_test.go
+++ pkg/controller/node/node_controller_test.go
@@ -17,10 +17,10 @@ limitations under the License.
 package node
 
 import (
+	"context"
 	"testing"
 	"time"
 
-	"golang.org/x/net/context"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
