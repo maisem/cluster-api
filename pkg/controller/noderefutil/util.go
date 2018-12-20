d5c61dc3ad28d2bf84b6d890941f01ca681c21ee
Maisem Ali <3953239+maisem@users.noreply.github.com>
2018-12-19 18:36:35 -0800

    Use machine status instead of node status.

--- pkg/controller/noderefutil/util.go
+++ pkg/controller/noderefutil/util.go
@@ -21,27 +21,43 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	clusterv1 "sigs.k8s.io/cluster-api/pkg/apis/cluster/v1alpha1"
 )
+
+func IsMachineAvailable(m *clusterv1.Machine, minReadySeconds int32, now metav1.Time) bool {
+	if m == nil {
+		return false
+	}
+	return readyForAtleast(m.Status.Conditions, minReadySeconds, now)
+}
 
 // IsNodeAvailable returns true if the node is ready and minReadySeconds have elapsed or is 0. False otherwise.
 func IsNodeAvailable(node *corev1.Node, minReadySeconds int32, now metav1.Time) bool {
-	if !IsNodeReady(node) {
+	if node == nil {
 		return false
 	}
+	return readyForAtleast(node.Status.Conditions, minReadySeconds, now)
+}
 
+func readyForAtleast(cs []corev1.NodeCondition, minReadySeconds int32, now metav1.Time) bool {
+	rc := getReadyCondition(cs)
+	if !isTrue(rc) {
+		return false
+	}
 	if minReadySeconds == 0 {
 		return true
 	}
+	x := time.Duration(minReadySeconds) * time.Second
+	return !rc.LastTransitionTime.IsZero() && rc.LastTransitionTime.Add(x).Before(now.Time)
+}
 
-	minReadySecondsDuration := time.Duration(minReadySeconds) * time.Second
-	readyCondition := GetReadyCondition(&node.Status)
-
-	if !readyCondition.LastTransitionTime.IsZero() &&
-		readyCondition.LastTransitionTime.Add(minReadySecondsDuration).Before(now.Time) {
-		return true
+func getReadyCondition(cs []corev1.NodeCondition) *corev1.NodeCondition {
+	for i := range cs {
+		if cs[i].Type == corev1.NodeReady {
+			return &cs[i]
+		}
 	}
-
-	return false
+	return nil
 }
 
 // GetReadyCondition extracts the ready condition from the given status and returns that.
@@ -50,23 +66,24 @@ func GetReadyCondition(status *corev1.NodeStatus) *corev1.NodeCondition {
 	if status == nil {
 		return nil
 	}
-	for i := range status.Conditions {
-		if status.Conditions[i].Type == corev1.NodeReady {
-			return &status.Conditions[i]
-		}
+	return getReadyCondition(status.Conditions)
+}
+
+func IsMachineReady(m *clusterv1.Machine) bool {
+	if m == nil {
+		return false
 	}
-	return nil
+	return isTrue(getReadyCondition(m.Status.Conditions))
+}
+
+func isTrue(rc *corev1.NodeCondition) bool {
+	return rc != nil && rc.Status == corev1.ConditionTrue
 }
 
 // IsNodeReady returns true if a node is ready; false otherwise.
 func IsNodeReady(node *corev1.Node) bool {
-	if node == nil || &node.Status == nil {
+	if node == nil {
 		return false
 	}
-	for _, c := range node.Status.Conditions {
-		if c.Type == corev1.NodeReady {
-			return c.Status == corev1.ConditionTrue
-		}
-	}
-	return false
+	return isTrue(getReadyCondition(node.Status.Conditions))
 }
